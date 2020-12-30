b3c33756fdadbb3196ce25249f9617085adb0374
Исаев Дмитрий <ice988@mail.ru>
2020-12-30 08:36:01 +0600

    Add files

---
+++ main.py
@@ -0,0 +1,14 @@
+import os
+list_ = []
+list_name = os.listdir('sorted')
+for i in list_name:
+    with open(f'sorted/{i}', encoding='utf-8') as f:
+        lines = f.readlines()
+        lines = [i + '\n'] + [str(len(lines)) + '\n'] + lines
+        list_.append(lines)
+
+text = ' '.join(sum((sorted(list_, key=len)), []))
+
+
+with open('all.txt', 'w', encoding='utf-8') as m:
+    m.write(text)
